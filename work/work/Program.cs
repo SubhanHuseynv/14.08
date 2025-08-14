58f4cdd0d011fd07b81db4d3e2016b03853120f7
Subhan Sh Huseynov <subhanshh@code.local>
2025-08-14 16:26:01 +0400

    the first'

---
+++ work/work/Program.cs
@@ -0,0 +1,20 @@
+﻿using work.Utilities.Extensions;
+
+namespace work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -93;
+            string b = "A3bhfh7676";
+            Console.WriteLine(a.IsOdd());
+            Console.WriteLine(a.IsEven());
+            Console.WriteLine(b.HasDigit());
+            Console.WriteLine(b.CheckPassword());
+            Console.WriteLine("slaldfkm".Capitalize());
+            
+        }
+    }
+}
+
---
+++ work/work/Utilities/Extensions/Helper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work.Utilities.Extensions
+{
+    internal static class Helper
+    {
+        public static bool IsOdd(this int num)
+        {
+            if (num == 0) return false;
+            if (num % 2 == 0) return true;
+            return false;
+        }
+        public static bool IsEven(this int num)
+        {
+            if (num == 0) return false;
+            if (num % 2 == 1 || num%2==-1) return true;
+            return false;
+        }
+        public static bool HasDigit(this string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        public static bool CheckPassword(this string str)
+        {
+            bool IsDig=false;
+            bool IsUp=false;
+            bool IsDown = false;
+            if (str.Length > 8)
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsDigit(str[i]))
+                    {
+                        IsDig = true;
+                    }
+                    if (char.IsUpper(str[i]))
+                    {
+                        IsUp = true;
+                    }
+                    if (char.IsLower(str[i]))
+                    {
+                        IsDown = true;
+                    }
+
+                }
+                if(IsDig && IsUp && IsDown){
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        public static string Capitalize(this string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(str.Substring(0, 1).ToUpper() + str.Substring(1).ToLower());
+            return sb.ToString();
+        }
+    }
+}
+
